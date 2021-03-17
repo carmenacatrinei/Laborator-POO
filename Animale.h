e866812372c4a6cc897a015db1606fad55bbd9c7
carmenacatrinei <73901669+carmenacatrinei@users.noreply.github.com>
2021-03-17 21:39:41 +0200

    Update Animale.h

--- Animale.h
+++ Animale.h
@@ -1,22 +1,25 @@
 #pragma once
 #include "Animal.h"
+#include <iostream>
 
 class Leu :public Animal {
 public:
-    Leu(std::string, int, bool, bool);
+    Leu(std::string, int,std::string, bool);
     ~Leu();
+    Leu(Leu copie_leu);
+    Leu operator=(const Leu& copie_leu);
+    friend ostream& operator<<(ostream& output, const Leu& afisare_leu);
     void dreseaza();
     void infoGenerale();
-    void afisare();
+    /*void afisare();*/
 };
 
 class Urs :public Animal {
 public:
-    Urs(std::string, int, bool, bool);
+    Urs(std::string, int, std::string, bool);
     ~Urs();
     void dreseaza();
     void infoGenerale();
     void afisare();
 };
-
 
