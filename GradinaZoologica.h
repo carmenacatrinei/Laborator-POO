be3c107c052248ffea3a3dcc69daca6f9d118cdc
carmenacatrinei <73901669+carmenacatrinei@users.noreply.github.com>
2021-03-19 20:23:41 +0200

    Update GradinaZoologica.h

--- GradinaZoologica.h
+++ GradinaZoologica.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Angajati.h"
 #include "Animale.h"
+#include <vector>
 
 class GradinaZoologica
 {
@@ -10,6 +11,57 @@ private:
 	friend class Dresor;
 	static const int salariuBaza_casier = 1500;
 	static const int salariuBaza_dresor = 1800;
-	static const int salariuBaza_ingrijitor = 1500;
+	static const int salariuBaza_ingrijitor = 1500; 
+	std::vector <Animal*> animale;
+	std::vector <Angajat*> angajati;
+
+public:
+	GradinaZoologica()
+	{}
+
+	~GradinaZoologica() 
+	{}
+
+	void adaugare_animal(Leu animal)
+	{
+		animale.push_back(new Leu(animal));
+	}
+
+	void adaugare_animal(Urs animal)
+	{
+		animale.push_back(new Urs(animal));
+	}
+
+/*	void adaugare_angajat(Casier angajat)
+	{
+		angajati.push_back(new Casier(angajat));
+	}
+
+	void adaugare_angajat(Ingrijitor angajat)
+	{
+		angajati.push_back(new Ingrijitor(angajat));
+	}
+
+	void adaugare_angajat(Dresor angajat)
+	{
+		angajati.push_back(new Dresor(angajat));
+	} //pentru o implementare ulterioara '\_(:D)_/'
+	*/
+	void afisare_zoo()
+	{
+		std::cout << "\nAnimalele sunt: " << "\n";
+		for (int i = 0; i < animale.size(); i++)
+		{
+			animale[i]->afisare();
+		}
+		std::cout << "\n\n";
+		/*std::cout << "Angajatii sunt: " << "\n";
+		for (int i = 0; i < angajati.size(); i++)
+		{
+			angajati[i]->afisare();
+		}*/
+	}
 };
+
+
 
