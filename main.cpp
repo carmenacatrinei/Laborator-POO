00844862f4e5138b3af5f42919c9026a8b0a2f61
carmenacatrinei <73901669+carmenacatrinei@users.noreply.github.com>
2021-03-28 15:33:34 +0300

    UPDATE

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@
 
 int main()
 {
-    Leu leu1("Charile", 4, 0, 'M');
-    Urs urs1("Bruno", 2, 1, 'F');
+    Leu leu1("Charile", 4, 0, 0);
+    Urs urs1("Bruno", 2, 1, 1);
 
-    cout << leu1;
+    std::cout << leu1;
     std::cout << "\n";
     leu1.dreseaza();
     std::cout << "\n";
-    cout << leu1;
+    std::cout << leu1;
     std::cout << "\n\n";
     urs1.afisare();
     std::cout << "\n";
@@ -20,10 +20,16 @@ int main()
     std::cout << "\n";
     urs1.afisare(); std::cout << "\n";
     std::cout << "\n";
-    Casier casier1("Popescu", "Dan", 30, 5, 1500);
+    Casier casier1("Popescu", "Dan", 30, 5);
     casier1.afisare();
     std::cout << "\n";
     casier1.marire_salariu();
     std::cout << "\n";
+
+    GradinaZoologica zoo1;
+    zoo1.adaugare_animal(leu1);
+    zoo1.adaugare_animal(urs1);
+    //zoo1.adaugare_angajat(casier1); 
+    zoo1.afisare_zoo();
 }
 
